d4a90d73ad6f9bc5c56e18294d608c9de0f3a372
arif-sk <arif.hossain177@gmail.com>
2019-01-29 14:33:20 +0600

    Update Calculator.cs

--- ConsoleApp1/ConsoleApp1/Calculator.cs
+++ ConsoleApp1/ConsoleApp1/Calculator.cs
@@ -8,12 +8,9 @@ namespace ConsoleApp1
 {
     class Calculator
     {
-        public int Add()
+        public int Add(var num1,var num2)
         {
-            int num1 = 0;
-            int num2 = 0;
-            int sum = num1 + num2;
-            return sum;
+            return num1+num2;
         }
     }
 }
